a2172b42f2fa01f87a236d89b8d9ff9554c63c53
SimonDeconde <simon.deconde@gmail.com>
2025-02-28 09:26:31 -0800

    Fix modal disclosure

--- apps/web/components/QuickUserSwitchModal.tsx
+++ apps/web/components/QuickUserSwitchModal.tsx
@@ -43,7 +43,7 @@ export const QuickUserSwitchModal = ({
   });
 
   return (
-    <Modal isOpen={disclosure.isOpen} onOpenChange={disclosure.onOpenChange}>
+    <Modal isOpen={disclosure.isOpen} onOpenChange={disclosure.onChange}>
       <ModalContent>
         {(onClose) => (
           <>
